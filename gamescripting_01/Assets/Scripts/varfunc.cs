2d5996fb541c371d015493cb7685907b7efa2cfc
Ben Shurts <benjaminshurts@gmail.com>
2018-09-19 16:19:03 -0600

    quiz

---
+++ gamescripting_01/Assets/Scripts/varfunc.cs
@@ -0,0 +1,23 @@
+//varfunc
+
+public int health = 100;
+private bool cool;
+public float ground;
+
+void start() {
+    ground = 0.5;
+    cool = 1;
+    health = 100;
+}
+
+void update() {
+    ground = 0.5;
+    cool = 1;
+    health = 100;
+}
+
+void fixedupdate() {
+    ground = 0.5;
+    cool = 1;
+    health = 100;
+}
