686a170097e537b171820dd064118639fd7cd3e4
Ben Shurts <benjaminshurts@gmail.com>
2018-09-24 17:19:10 -0600

    Score

--- gamescripting_01/Assets/Scripts/CharacterMove.cs
+++ gamescripting_01/Assets/Scripts/CharacterMove.cs
@@ -6,12 +6,16 @@ public class CharacterMove : MonoBehaviour {
 	// Player Movement Variables
 	public int MoveSpeed;
 	public float JumpHeight;
+	private bool DoubleJump;
 
 	//Player grounded variables
 	public Transform groundCheck;
 	public float groundCheckRadius;
 	public LayerMask whatIsGround;
 	private bool grounded;
+
+	//non-stick player
+	private float MoveVelocity;
 
 	// Use this for initialization
 	void Start () {
@@ -27,19 +31,32 @@ public class CharacterMove : MonoBehaviour {
 	void Update () {
 
 		// jump
-		if(Input.GetKeyDown (KeyCode.Space)&& grounded){
+		if(Input.GetKeyDown (KeyCode.Space) && grounded){
 			Jump();
 		}
+		//double jump
+		if(grounded) 
+			DoubleJump = false;
 
+		if(Input.GetKeyDown (KeyCode.Space) && !DoubleJump && !grounded) {
+			Jump();
+			DoubleJump = true;
+		}
+
+		//non-stick player
+		MoveVelocity = 0f;
+		
 		// move
 		if(Input.GetKey (KeyCode.D)){
-			GetComponent<Rigidbody2D>().velocity = new Vector2(MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
-			
+			// GetComponent<Rigidbody2D>().velocity = new Vector2(MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+			MoveVelocity = MoveSpeed;
 		}
 		if(Input.GetKey (KeyCode.A)){
-			GetComponent<Rigidbody2D>().velocity = new Vector2(-MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
-			
+			// GetComponent<Rigidbody2D>().velocity = new Vector2(-MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+			MoveVelocity = -MoveSpeed;
 		}
+
+		GetComponent<Rigidbody2D>().velocity = new Vector2(MoveVelocity, GetComponent<Rigidbody2D>().velocity.y);
 
 	}
 
